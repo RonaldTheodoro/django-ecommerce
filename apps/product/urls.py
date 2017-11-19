39f1738e4f2581fb50f2050ce4ea0bf4f050b5ed
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 15:06:24 -0200

    use context to retrive product list

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^list/$', views.product_list, name='list'),
+    url(r'^list/$', views.ProductListView.as_view(), name='list'),
 ]
--- apps/product/views.py
+++ apps/product/views.py
@@ -6,8 +6,12 @@ from . import models
 
 class ProductListView(generic.ListView):
     queryset = models.Product.objects.all()
+    template_name = 'product/list.html'
+
+    def get_context_data(self, *args, **kwargs):
+        return super(ProductListView, self).get_context_data(*args, **kwargs)
 
 
 def product_list(request):
     products = models.Product.objects.all()
-    return render(request, 'product/list.html', {'products': products})
+    return render(request, 'product/list.html', {'product_list': products})

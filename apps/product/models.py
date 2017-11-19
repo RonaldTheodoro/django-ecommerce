da4619690f623a3bc0b9de71d01ca947681b26b8
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 14:35:49 -0200

    add Product model and migrations

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField('title', max_length=120)
+    descripition = models.TextField('descripition')
+    price = models.DecimalField(
+        'price',
+        decimal_places=2,
+        max_digits=20,
+        default=39.99
+    )
+
+    def __str__(self):
+        return self.title

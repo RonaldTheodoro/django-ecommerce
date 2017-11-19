5d926a83861b79f45e14dbb1cf2bcbd46121581a
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 15:09:02 -0200

    use object list to retrive products list

--- apps/product/views.py
+++ apps/product/views.py
@@ -8,9 +8,6 @@ class ProductListView(generic.ListView):
     queryset = models.Product.objects.all()
     template_name = 'product/list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        return super(ProductListView, self).get_context_data(*args, **kwargs)
-
 
 def product_list(request):
     products = models.Product.objects.all()

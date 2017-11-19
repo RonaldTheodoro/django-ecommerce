cba10dcac94145c94717d7d05a6eebf0e59f0ac8
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 14:53:18 -0200

    add product list view

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,13 @@
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+class ProductListView(generic.ListView):
+    queryset = models.Product.objects.all()
+
+
+def product_list(request):
+    products = models.Product.objects.all()
+    return render(request, 'product/list.html', {'products': products})
--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.core.urls', namespace='core')),
+    url(r'^product/', include('apps.product.urls', namespace='product')),
     url(r'^account/', include('apps.account.urls', namespace='account')),
 ]

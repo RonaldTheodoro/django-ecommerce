38df1d8e4fcd9c295614cbdec05d2d45f5f3d65b
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 16:05:32 -0200

    add ProductListViewTest, ProductDetailViewTest and ProductFailDetailViewTest

--- apps/product/tests.py
+++ apps/product/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from . import models
+
+
+class ProductListViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('product:list'))
+
+    def test_get(self):
+        """GET /product/list must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """GET /product/list must use product/list.html template"""
+        self.assertTemplateUsed(self.response, 'product/list.html')
+
+
+class ProductDetailViewTest(TestCase):
+
+    def setUp(self):
+        models.Product.objects.create(
+            title='hat',
+            descripition='this is an awesome hat',
+            price=59.99
+        )
+        self.response = self.client.get(
+            reverse('product:detail', kwargs={'pk': 1})
+        )
+
+    def test_get(self):
+        """GET /product/list must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """GET /product/list must use product/list.html template"""
+        self.assertTemplateUsed(self.response, 'product/detail.html')
+
+
+class ProductFailDetailViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(
+            reverse('product:detail', kwargs={'pk': 99})
+        )
+
+    def test_get(self):
+        """GET /product/list must return status code 200"""
+        self.assertEqual(404, self.response.status_code)

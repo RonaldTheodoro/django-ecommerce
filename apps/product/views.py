5ec57669ae3e4f80c450de699401f43ef6120fed
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 18:28:27 -0200

    remove FBV

--- apps/product/views.py
+++ apps/product/views.py
@@ -9,19 +9,9 @@ class ProductListView(generic.ListView):
     template_name = 'product/list.html'
 
 
-def product_list(request):
-    products = models.Product.objects.all()
-    return render(request, 'product/list.html', {'product_list': products})
-
-
 class ProductDetailView(generic.DetailView):
     queryset = models.Product.objects.all()
     template_name = 'product/detail.html'
 
     def get_context_data(self, *args, **kwargs):
         return super(ProductDetailView, self).get_context_data(*args, **kwargs)
-
-
-def detail(request, pk):
-    product = get_object_or_404(models.Product, pk=pk)
-    return render(request, 'product/detail.html', {'product': product})

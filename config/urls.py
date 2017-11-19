02cb59362ce8c4748ee5d8db563c5611f600f7b3
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-11-19 17:31:18 -0200

    add static and media urls (only in debugging mode)

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,3 +9,10 @@ urlpatterns = [
     url(r'^product/', include('apps.product.urls', namespace='product')),
     url(r'^account/', include('apps.account.urls', namespace='account')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

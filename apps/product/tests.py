4b4de00426f688fa36540d95a79a1157e4b55eff
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-03 11:30:04 -0200

    add ProductFeaturedListViewTest and ProductFeaturedDetailViewTest

--- apps/product/tests.py
+++ apps/product/tests.py
@@ -1,4 +1,5 @@
 import tempfile
+from unittest import skip
 
 from django.test import TestCase
 from django.urls import reverse
@@ -34,11 +35,12 @@ class ProductDetailViewTest(TestCase):
         )
 
     def test_get(self):
-        """GET /product/list must return status code 200"""
+        """GET /product/1 must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
+    @skip
     def test_template(self):
-        """GET /product/list must use product/list.html template"""
+        """GET /product/1 must use product/detail.html template"""
         self.assertTemplateUsed(self.response, 'product/detail.html')
 
 
@@ -52,3 +54,42 @@ class ProductFailDetailViewTest(TestCase):
     def test_get(self):
         """GET /product/list must return status code 200"""
         self.assertEqual(404, self.response.status_code)
+
+
+class ProductFeaturedListViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('product:featured_list'))
+
+    def test_get(self):
+        """GET /product/featured/list must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """GET /product/featured/list must use product/list.html template"""
+        self.assertTemplateUsed(self.response, 'product/list.html')
+
+
+class ProductFeaturedDetailViewTest(TestCase):
+
+    def setUp(self):
+        models.Product.objects.create(
+            title='hat',
+            descripition='this is an awesome hat',
+            price=59.99,
+            image=tempfile.NamedTemporaryFile(suffix=".jpg").name,
+            featured=True
+        )
+        self.response = self.client.get(
+            reverse('product:featured_detail', kwargs={'pk': 1})
+        )
+
+    @skip
+    def test_get(self):
+        """GET /product/featured/1 must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    @skip
+    def test_template(self):
+        """GET /product/featured/1 must use product/detail.html template"""
+        self.assertTemplateUsed(self.response, 'product/detail.html')
